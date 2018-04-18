1e9840235d4639d8df07ffda662c316d0289b89c
Antoine <antoine.cervo@gmail.com>
2018-04-18 20:28:26 +0200

    Update dataBaseConnect.php

--- dataBaseConnect.php
+++ dataBaseConnect.php
@@ -2,17 +2,12 @@
 
 function dbConnect(){
 
-/*
+
   $servername="localhost";
   $username="root";
   $password="";
   $dbname="pokeapi";
-  */
-
-  $servername="mysql.hostinger.com";
-  $username = "u267870526_mejn";
-  $dbname="u267870526_me";
-  $password="Bue2nVwLPoN2";
+  
 
   try {
       $bdd = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
